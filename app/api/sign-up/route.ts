01150c3cc6e619c1b5500190ee757f95558f58d1
Prashant <prashantsoni1242@gmail.com>
2024-04-09 22:50:03 +0530

    signup route handler validation

--- app/api/sign-up/route.ts
+++ app/api/sign-up/route.ts
@@ -1,7 +1,13 @@
+import { SignupSchema } from "@/schemas";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const body = await req.json();
+  const validatedData = SignupSchema.safeParse(body);
+
+  if (!validatedData.success) {
+    return NextResponse.json({ error: "Invalid fields" }, { status: 500 });
+  }
 
   return NextResponse.json({ response: "success" }, { status: 200 });
 }
