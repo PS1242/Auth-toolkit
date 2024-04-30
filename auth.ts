a7c0db9cef80c1ccad3c5fd7bf75a72194a78cfc
Prashant <prashantsoni1242@gmail.com>
2024-04-30 23:59:03 +0530

    extends session and adds role and id to it

--- auth.ts
+++ auth.ts
@@ -1,13 +1,64 @@
-import NextAuth from "next-auth";
+import NextAuth, { User, DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./lib/prismaclient";
 import Credentials from "next-auth/providers/credentials";
 import { LoginSchema } from "./schemas";
 import bcrypt from "bcryptjs";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth" {
+  interface User {
+    // Add your additional properties here:
+    role: string;
+  }
+}
+
+declare module "@auth/core/adapters" {
+  interface AdapterUser {
+    // Add your additional properties here:
+    role: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    id: string;
+    role: string;
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
+  callbacks: {
+    jwt({ token, user }) {
+      if (user && user.id) {
+        token.id = user.id;
+      }
+      if (user && user.role) {
+        token.role = user.role;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      console.log("my token", token);
+      if (session.user) {
+        session.user.id = token.id;
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
   providers: [
     Credentials({
       authorize: async (creds) => {
