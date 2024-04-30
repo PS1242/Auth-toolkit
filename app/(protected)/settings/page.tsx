00617d40de0e291c41370995affaed7620c011a7
Prashant <prashantsoni1242@gmail.com>
2024-04-30 20:57:05 +0530

    adds signout on the settings page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,9 +1,21 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 const Settings = async () => {
   const session = await auth();
 
-  return <div className="p-8">{JSON.stringify(session)}</div>;
+  return (
+    <div className="p-8">
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign out</button>
+      </form>
+    </div>
+  );
 };
 
 export default Settings;
