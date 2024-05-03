98ca5d05090db7e464c5899d719745951af0f8f4
Prashant <prashantsoni1242@gmail.com>
2024-05-04 00:01:55 +0530

    social login icons

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -20,6 +20,8 @@ import { z } from "zod";
 import FormState from "@/components/ui/form-state";
 import { useRouter } from "next/navigation";
 import { DEFAULT_LOGIN_REDIRECT_URL } from "@/routes";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 export default function SignInPage() {
   const form = useForm<z.infer<typeof LoginSchema>>({
@@ -93,6 +95,25 @@ export default function SignInPage() {
             {response && (
               <FormState type={response?.type} message={response?.message} />
             )}
+            {/* Social login buttons */}
+            <div className="flex gap-2">
+              <Button
+                size={"lg"}
+                variant={"outline"}
+                className="w-full"
+                onClick={() => {}}
+              >
+                <FcGoogle className="w-5 h-5" />
+              </Button>
+              <Button
+                size={"lg"}
+                variant={"outline"}
+                className="w-full"
+                onClick={() => {}}
+              >
+                <FaGithub className="w-5 h-5" />
+              </Button>
+            </div>
             <Button className="w-full" type="submit" disabled={loading}>
               Login
             </Button>
