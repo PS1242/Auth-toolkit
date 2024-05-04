a86968d44f0bc83d36d9717b6650803431945564
Prashant <prashantsoni1242@gmail.com>
2024-05-04 17:54:29 +0530

    home page redirect and auth icon

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,11 +1,26 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { FcLock } from "react-icons/fc";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-sky-100">
-      <h1 className="font text-3xl text-gray-800 mb-8">Auth Toolkit</h1>
+      <div
+        className="flex items-end gap-2 mb-8 cursor-pointer"
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        <FcLock className="w-10 h-10" />
+        <h1 className="font text-3xl text-gray-800 ">Auth Toolkit</h1>
+      </div>
       {children}
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
+import { FcLock } from "react-icons/fc";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-sky-100 flex justify-center items-center">
       <div className={cn("text-center")}>
-        <h1 className="font text-3xl text-gray-800 mb-8">Auth Toolkit</h1>
+        <div className="flex gap-2 items-end mb-8">
+          <FcLock className="w-10 h-10" />
+          <h1 className="font text-3xl text-gray-800">Auth Toolkit</h1>
+        </div>
         <Button className="p-9 text-2xl group rounded-full" asChild>
           <Link href={"/auth/sign-in"}>
             <h2 className="flex gap-1 items-center">
